072806c8db6713f764db760e151a670a1a758ebb
Tortooga <oazeem37@gmail.com>
2023-10-07 18:54:55 +0300

    introducing salting and improving cycling
    introducing salting which sprinkles in some random elements such that they may cloud how long the orignal message is, as well as confuse any interceptors, and it will make the proccess of breaking the encryption way harder as mistaking one salt value to be a true value will desync the key by cycling, which will disable any interceptors from reading the encrypted message.
    
    issues:
    salts are sprinkled in uniformly, which means if a person some how knows exactly how the random values are scattered this may make it easier for them to break the encryption(although it is still borderline impossible to be broken unless the interceptor knows the keyvas the cycling mechenism makes bruteforcing it astronomically long).
    I should find a less predictable way to scatter the values rather than uniformly in encremments governed by the value of the variable saltJump, which similar to cyclejump is governed by the key.

--- encryptionSys.cs
+++ encryptionSys.cs
@@ -5,7 +5,7 @@ namespace encryptionSys // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static char[] alpha = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890- ,.='@#~/?".ToCharArray();
+        static char[] alpha = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890- ;,.='@#~/?".ToCharArray();
         static Random random = new Random();
 
         static void Main(string[] args)
@@ -81,24 +81,31 @@ namespace encryptionSys // Note: actual namespace depends on the project name.
         static char[] encrypt(string original, char[] key){
             original.ToCharArray();
 
+            int jumpConstant = Array.IndexOf(alpha,key[0]);
+            if (jumpConstant == 0){
+                jumpConstant = 1;
+            }
+
+            int saltJump = Array.IndexOf(alpha,key[0]) + 1; //added 1 such that it is never 0
+            if (saltJump < 2){
+                saltJump = 2;
+            }
+            else if (saltJump > 4){
+                saltJump = 2;
+            }
             char[] encrypted = new char[original.Length];
             for (int i = 0; i < original.Length; i++)
             {
                 encrypted[i] = key[Array.IndexOf(alpha,original[i])];
-                key = cycle(key);
+                key = cycle(key, jumpConstant);
             }
 
-            return encrypted;
+            return salt(encrypted, saltJump);
         }
 
-        static char[] cycle(char[] inSeq){
+        static char[] cycle(char[] inSeq, int jumpConstant){
             char[] fiSeq = new char[inSeq.Length];
-            int jumpConstant = Array.IndexOf(alpha,inSeq[0]);
-
             fiSeq[jumpConstant] = inSeq[0];
-            if (jumpConstant == 0){
-                jumpConstant = 1;
-            }
 
             for (int i = 1; i < inSeq.Length; i++)
             {
@@ -108,15 +115,64 @@ namespace encryptionSys // Note: actual namespace depends on the project name.
         }
 
         static char[] decrypt(char[] encrypted, char[] key){
-            
+            int jumpConstant = Array.IndexOf(alpha,key[0]);
+            if (jumpConstant == 0){
+                jumpConstant = 1;
+            }
+
+            int saltJump = Array.IndexOf(alpha,key[0]) + 1; //added 1 such that it is never 0
+            if (saltJump < 2){
+                saltJump = 2;
+            }
+            else if (saltJump > 4){
+                saltJump = 2;
+            }
+
+            encrypted = unsalt(encrypted, saltJump);
+
             char[] decrypted = new char[encrypted.Length];
             for (int i = 0; i < encrypted.Length; i++)
             {
                 decrypted[i] = alpha[Array.IndexOf(key,encrypted[i])];
-                key = cycle(key);
+                key = cycle(key,jumpConstant);
             }
 
             return decrypted;
+        }
+
+        static char[] salt(char[] unsalted,int saltJump){
+            char[] salted = new char[unsalted.Length + floorDiv(unsalted.Length,saltJump - 1) + 1];
+
+            int k = 0;
+            for (int i = 0; i < salted.Length; i++)
+            {
+                if (i % saltJump == 0){
+                    salted[i] = alpha[choose(alpha)];
+                }
+                else{
+                    salted[i] = unsalted[k];
+                    k++;
+                }  
+            }
+
+            return salted;
+        }
+
+        static char[] unsalt(char[] salted, int saltJump){
+            char[] unsalted = new char[salted.Length - floorDiv(salted.Length,saltJump) - 1];
+            int k = 0;
+            for (int i = 0; i < salted.Length; i++)
+            {
+                if (i % saltJump == 0){
+                    continue;
+                }
+                else{
+                    unsalted[k] = salted[i];
+                    k++;
+                }
+            }
+
+            return unsalted;
         }
 
         static int cycleReset(int num){
@@ -127,6 +183,11 @@ namespace encryptionSys // Note: actual namespace depends on the project name.
                 return num;
             }
         }
+
+        static int floorDiv(int dividend, int divisor){
+            return (dividend - (dividend % divisor)) / divisor;
+        }
+
     }
 }
 
